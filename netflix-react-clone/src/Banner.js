09afae48d574fb3eeaeeb427deef8d095369bab5
Banadzem Berinyny Valery <tikavalery@gmail.com>
2020-10-04 16:38:54 -0700

    added code for netflix clone

---
+++ netflix-react-clone/src/Banner.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import "./Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      console.log(request);
+      console.log("Yess");
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+  console.log(`
+    https://image.tmdb.org/t/p/original/${movie?.backdrop_path}
+`);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+            https://image.tmdb.org/t/p/original/${movie?.backdrop_path}
+        )`,
+      }}
+    >
+      <div className="banner__contents">
+        {/* { optional chaining used to check if onject property comes as either title,name or original_name} */}
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom"></div>
+    </header>
+  );
+}
+
+export default Banner;
---
+++ netflix-react-clone/src/Row.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+// import Requests from "./Requests";
+import axios from "./axios";
+import "./Row.css";
+
+//  tmdb base url to be added to info coming from the data fom the API request from tmdb
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  // console.table(movies);
+  return (
+    <div className="row">
+      <h1>{title}</h1>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
